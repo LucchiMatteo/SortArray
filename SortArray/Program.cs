7a4d136ecaea95b9f1ff5f23c3281912be86ea4e
LucchiMatteo <mlucchi79@gmail.com>
2021-09-24 08:29:29 +0200

    V1.0: creazione array e ordinamento

---
+++ SortArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vect = new int[10] { 5, 78, -8, 41, 0, 74, 6, 38, -7, -85 };
+
+            Array.Sort(vect);
+        }
+    }
+}
