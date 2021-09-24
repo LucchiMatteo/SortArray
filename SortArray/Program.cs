db0aea8effa9f88b3e2ab1f0c11c6247c0f9a857
LucchiMatteo <mlucchi79@gmail.com>
2021-09-24 08:44:08 +0200

    V1.1: aggiunta metodo stampa elementi array

--- SortArray/Program.cs
+++ SortArray/Program.cs
@@ -9,6 +9,14 @@ namespace SortArray
             int[] vect = new int[10] { 5, 78, -8, 41, 0, 74, 6, 38, -7, -85 };
 
             Array.Sort(vect);
+
+            Print(vect);
+        }
+
+        static void Print (int [] v)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(v[i]);
         }
     }
 }
